558314d25fa467767a26295a3370eb848b6a0332
Popo <147984101+Popoisthebest@users.noreply.github.com>
2024-11-04 18:52:12 +0900

    [feat] 최종4차

--- js/reservationForm.js
+++ js/reservationForm.js
@@ -26,18 +26,20 @@ document.addEventListener("DOMContentLoaded", function() {
 document.getElementById("reservation-form").addEventListener("submit", async function(event) {
     event.preventDefault();
     const club = document.getElementById("club").value;
-    const student = document.getElementById("student").value;
-    const contact = `${document.getElementById("contact-prefix").value}-${document.getElementById("contact-middle").value}-${document.getElementById("contact-last").value}`;
+    const studentId = document.getElementById("student-id").value;
+    const studentName = document.getElementById("student-name").value;
+    const student = `${studentId} - ${studentName}`;
+    const contact = `010-${document.getElementById("contact-middle").value}-${document.getElementById("contact-last").value}`;
     const email = document.getElementById("email").value;
     const purpose = document.getElementById("purpose").value;
 
     const selectionData = JSON.parse(localStorage.getItem("reservationSelection"));
-    const { rooms, date } = selectionData;
-
     if (!selectionData) {
         alert("예약할 장소와 시간을 먼저 선택하세요.");
         return;
     }
+
+    const { rooms, date } = selectionData;
 
     // Firestore에 각 선택된 방과 시간 저장
     try {
@@ -68,5 +70,6 @@ document.getElementById("reservation-form").addEventListener("submit", async fun
         window.location.href = './calendar.html'; // 예약 완료 후 캘린더 페이지로 이동
     } catch (error) {
         console.error("예약 저장 오류:", error);
+        alert("예약 중 오류가 발생했습니다. 다시 시도해 주세요.");
     }
 });
